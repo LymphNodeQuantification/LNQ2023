3c4919a1df27e5eaa84f2a4ff9316985ca29a957
Roya Khajavi <royakhajavi@gmail.com>
2023-08-05 04:06:25 +0000

    update algorithm

--- Algorithm/process.py
+++ Algorithm/process.py
@@ -12,9 +12,12 @@ from evalutils.validators import (
 
 class Lnq2023(SegmentationAlgorithm):
     def __init__(self):
+        output_path=Path('/output/images/mediastinal-lymph-node-segmentation/')
+        if not output_path.exists():
+            output_path.mkdir(parents=True, exist_ok=True)
         super().__init__(
             input_path=Path('/input/images/mediastinal-ct/'),
-            output_path=Path('/output/images/mediastinal-lymph-node-segmentation/'),
+            output_path = output_path, 
             validators=dict(
                 input_image=(
                     UniqueImagesValidator(),
@@ -25,6 +28,7 @@ class Lnq2023(SegmentationAlgorithm):
 
     def predict(self, *, input_image: SimpleITK.Image) -> SimpleITK.Image:
         # Segment all values greater than 2 in the input image
+        print('abc')
         return SimpleITK.BinaryThreshold(
             image1=input_image, lowerThreshold=2, insideValue=1, outsideValue=0
         )
